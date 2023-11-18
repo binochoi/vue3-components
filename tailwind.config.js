d53a11efd6b3d06761c8d2d606c1aa85647bae30
bino <bonoself@gmail.com>
2023-11-18 19:48:32 +0900

    feat: apply tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./stories/**/*.{ts,vue}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
