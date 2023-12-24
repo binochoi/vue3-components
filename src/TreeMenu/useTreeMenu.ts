8f9115860809a46d7fb7ee268ae4576220013f1b
bino <bonoself@gmail.com>
2023-12-24 20:12:30 +0900

    style:

--- src/TreeMenu/useTreeMenu.ts
+++ src/TreeMenu/useTreeMenu.ts
@@ -1,14 +1,14 @@
-import type { Item } from './types';
+import type { MenuItem } from './types';
 
 export const useTreeMenu = () => {
   const useFiltering = () => {
-    const filterSearchedItem = (searchText: string) => (item: Item) => {
+    const filterSearchedItem = (searchText: string) => (item: MenuItem) => {
       const excludedSearchTextFromLabel = item.label && searchText && !item.label.toLowerCase().includes(searchText.toLowerCase());
       if (item.label === undefined || excludedSearchTextFromLabel) return false;
       return true;
     };
-    const recursiveFilterItem = (searchText: string) => (item: Item): Item | null => {
-      const children = item.children?.map(recursiveFilterItem(searchText)).filter((item): item is Item => item !== null);
+    const recursiveFilterItem = (searchText: string) => (item: MenuItem): MenuItem | null => {
+      const children = item.children?.map(recursiveFilterItem(searchText)).filter((item): item is MenuItem => item !== null);
       const isSearchedItem = filterSearchedItem(searchText)(item) || children?.length;
       if (!isSearchedItem) {
         return null;
@@ -18,7 +18,7 @@ export const useTreeMenu = () => {
         children,
       };
     };
-    const getSearchedItems = (items: Item[], searchText: string) => items.map(recursiveFilterItem(searchText)).filter((item): item is Item => item !== null);
+    const getSearchedItems = (items: MenuItem[], searchText: string) => items.map(recursiveFilterItem(searchText)).filter((item): item is MenuItem => item !== null);
     return {
       getSearchedItems,
     };
